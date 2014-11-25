0ee2183b27d4bbba7b10e723aec2034f5a49f36c
wbjari <wbjari@gmail.com>
2014-11-25 14:54:02 +0100

    Code van register.php naar registerController verplaatst.

---
+++ Site/views/register.view.php
@@ -0,0 +1,14 @@
+<form action="" method="post">
+  <input type="text" name="companyname">
+  <input type="text" name="residence">
+  <input type="text" name="firstname">
+  <input type="text" name="lastname">
+  <input type="text" name="insertion">
+  <input type="text" name="zipcode">
+  <input type="text" name="telephone">
+  <input type="email" name="email">
+  <input type="text" name="password">
+  <input type="text" name="street">
+  <input type="text" name="house_number">
+  <input type="submit">
+</form>

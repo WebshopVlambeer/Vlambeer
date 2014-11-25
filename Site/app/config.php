3b44d0a74f2e361951a1c47455b7133978f6d4e0
SvenLuijten <svenluytjes@hotmail.com>
2014-11-25 16:50:25 +0100

    Re-added config.php

---
+++ Site/app/config.php
@@ -0,0 +1,11 @@
+<?php
+	# DATABASE CONSTANTS
+	define("HOST", "localhost");																			# Host for the database.
+	define("DB_NAME", "vlambeer_webshop");														# Name of database you want to connect with.
+	define("USER", "root");																						# Username to connect to database.
+	define("PASS", "");																								# Password to connect to database.
+
+	# PATH CONTSTANTS
+	define("ROOT", "http://localhost/git/Vlambeer/site");							# Root of site.
+	define("ASSETS", ROOT."/assets");																	# Assets of site.
+?>

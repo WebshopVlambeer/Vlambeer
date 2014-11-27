55de7b13a76ee4dbae6a88b511f1d2b6548d9edf
Remmert <remmert_kloppenburg@msn.com>
2014-11-27 12:36:09 +0100

    Update database add tbl_invoices

--- Site/controllers/invoicesController.php
+++ Site/controllers/invoicesController.php
@@ -1,14 +1,11 @@
 <?php
-	session_start();
-	require_once "../app/config.php";
-	require_once "../app/functions.php";
-	require_once "../libs/Database.php";
-	require_once "../libs/User.php";
 
 
-	$result = $db->query("SELECT * FROM tbl_invoices");
 
-	echo '<td><a href="../invoices.php?customer_id='.$row['customer_id']. '">View</a></td>';
+ foreach($result as $invoic){
 
+	   echo '<td><a href="' . ROOT . '../invoices.php/'.$invoic->customer_id.'/"><div class="btn btn-primary">Edit</div></a></td>';
+
+}
 
 ?>

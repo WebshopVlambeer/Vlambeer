da840d890da796971e3ef9eafa0b8a8fd6ddf1f5
wbjari <wbjari@gmail.com>
2014-11-28 12:22:13 +0100

    Added style of the shop

---
+++ Site/views/shop.view.php
@@ -0,0 +1,154 @@
+<h1 class="productAreaTitle">Vlambeer Games</h1>
+<div class="productArea">
+	<div class="prevBtn">
+		<a href="#">
+			<img src="<?= ROOT."/assets/img/prevbtn.png" ?>"/>
+		</a>
+	</div>
+	<div class="products">
+		<div class="product1">
+			Product 1
+		</div>
+		<div class="product2">
+			Product 2
+		</div>
+		<div class="product3">
+			Product 3
+		</div>
+	</div>
+	<div class="nextBtn">
+		<a href="#">
+			<img src="<?= ROOT."/assets/img/nextbtn.png" ?>"/>
+		</a>
+	</div>
+</div>
+
+
+<h1 class="productAreaTitle">Special / Bundles</h1>
+<div class="productArea">
+	<div class="prevBtn">
+		<a href="#">
+			<img src="<?= ROOT."/assets/img/prevbtn.png" ?>"/>
+		</a>
+	</div>
+	<div class="products">
+		<div class="product1">
+			Product 1
+		</div>
+		<div class="product2">
+			Product 2
+		</div>
+		<div class="product3">
+			Product 3
+		</div>
+	</div>
+	<div class="nextBtn">
+		<a href="#">
+			<img src="<?= ROOT."/assets/img/nextbtn.png" ?>"/>
+		</a>
+	</div>
+</div>
+
+
+<h1 class="productAreaTitle">Clothes</h1>
+<div class="productArea">
+	<div class="prevBtn">
+		<a href="#">
+			<img src="<?= ROOT."/assets/img/prevbtn.png" ?>"/>
+		</a>
+	</div>
+	<div class="products">
+		<div class="product1">
+			Product 1
+		</div>
+		<div class="product2">
+			Product 2
+		</div>
+		<div class="product3">
+			Product 3
+		</div>
+	</div>
+	<div class="nextBtn">
+		<a href="#">
+			<img src="<?= ROOT."/assets/img/nextbtn.png" ?>"/>
+		</a>
+	</div>
+</div>
+
+
+<h1 class="productAreaTitle">Miscellaneous</h1>
+<div class="productArea">
+	<div class="prevBtn">
+		<a href="#">
+			<img src="<?= ROOT."/assets/img/prevbtn.png" ?>"/>
+		</a>
+	</div>
+	<div class="products">
+		<div class="product1">
+			Product 1
+		</div>
+		<div class="product2">
+			Product 2
+		</div>
+		<div class="product3">
+			Product 3
+		</div>
+	</div>
+	<div class="nextBtn">
+		<a href="#">
+			<img src="<?= ROOT."/assets/img/nextbtn.png" ?>"/>
+		</a>
+	</div>
+</div>
+
+
+<h1 class="productAreaTitle">Music</h1>
+<div class="productArea">
+	<div class="prevBtn">
+		<a href="#">
+			<img src="<?= ROOT."/assets/img/prevbtn.png" ?>"/>
+		</a>
+	</div>
+	<div class="products">
+		<div class="product1">
+			Product 1
+		</div>
+		<div class="product2">
+			Product 2
+		</div>
+		<div class="product3">
+			Product 3
+		</div>
+	</div>
+	<div class="nextBtn">
+		<a href="#">
+			<img src="<?= ROOT."/assets/img/nextbtn.png" ?>"/>
+		</a>
+	</div>
+</div>
+
+
+<h1 class="productAreaTitle">Not-Vlambeer Games</h1>
+<div class="productArea">
+	<div class="prevBtn">
+		<a href="#">
+			<img src="<?= ROOT."/assets/img/prevbtn.png" ?>"/>
+		</a>
+	</div>
+	<div class="products">
+		<div class="product1">
+			Product 1
+		</div>
+		<div class="product2">
+			Product 2
+		</div>
+		<div class="product3">
+			Product 3
+		</div>
+	</div>
+	<div class="nextBtn">
+		<a href="#">
+			<img src="<?= ROOT."/assets/img/nextbtn.png" ?>"/>
+		</a>
+	</div>
+</div>

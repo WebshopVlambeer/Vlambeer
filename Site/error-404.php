7ed01044b64a1094f82cac08cdf79ef2051a6883
SvenLuijten <svenluytjes@hotmail.com>
2014-11-28 11:11:15 +0100

    changed error page

--- Site/error-404.php
+++ Site/error-404.php
@@ -2,13 +2,13 @@
 	session_start();
 	$title = "Whoopsie!";
 
-	require_once "inc/header.php";
+	require "inc/header.php";
 
 	# Handling invalid pagevisits.
 
 	echo "oops!";
 	# Logic for error.php
 	
-	require_once "views/error.view.php";
-	require_once "inc/footer.php";
+	require "views/error.view.php";
+	require "inc/footer.php";
 ?>

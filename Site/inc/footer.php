8ba6fbdf1e4cd8eb87266e37ee232a3abf2e4bed
SvenLuijten <svenluytjes@hotmail.com>
2014-11-28 12:24:47 +0100

    footer

--- Site/inc/footer.php
+++ Site/inc/footer.php
@@ -1,5 +1,26 @@
 				</div> <!-- /.container -->
-			</div> <!-- /.content -->	
+			</div> <!-- /.content -->
+
+			<footer>
+				<div class="footer-top">
+					<div class="container">
+						<h2>Subscribe to our newsletter:</h2>
+						<form action="<?= ROOT ?>/controllers/newsletterController.php">
+							<input type="email" placeholder="Email address" name="newsletterEmail" autocomplete="on">
+							<input type="submit" hidden>
+						</form>
+					</div>
+				</div>
+
+				<div class="footer-bottom">
+					<div class="container">
+						<div class="footer-section">
+							<h2>About:</h2>
+							<p>VLAMBEER is a Dutch independent game studio made up of Rami Ismail and Jan Willem Nijman, bringing back arcade games since 1955.</p>
+						</div>
+					</div>
+				</div>
+			</footer>
 	
 		<!-- JavaScript -->
 		<script src="<?= ASSETS ?>/js/jquery.min.js"></script>

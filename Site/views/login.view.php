63ac202bcec1ad026255f1fd96682b3f0e70a3c6
Remmert <remmert_kloppenburg@msn.com>
2014-11-28 10:55:49 +0100

    Update login/invoices

--- Site/views/login.view.php
+++ Site/views/login.view.php
@@ -1,4 +1,4 @@
-<form method="post" action="<?= ROOT . '/shop/' ?>"  role="form" class="login-form col-md-4 col-md-offset-4">
+<form method="post" action="<?= ROOT . '/controllers/loginController.php' ?>"  role="form" class="login-form col-md-4 col-md-offset-4">
 	<fieldset>
 		<legend><h2>Login</h2></legend>
 

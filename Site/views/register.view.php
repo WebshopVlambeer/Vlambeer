768a12c0cc5b8fe65821e34ee47236e690c834bd
wbjari <wbjari@gmail.com>
2014-12-05 08:55:36 +0100

    update shop style + register style

--- Site/views/register.view.php
+++ Site/views/register.view.php
@@ -1,40 +1,60 @@
-<form action="<?= ROOT."/controllers/registerController.php" ?>" method="post">
-  <fieldset>
-    <legend>Login</legend>
+<form class="regForm" action="<?= ROOT."/controllers/registerController.php" ?>" method="post">
+  <legend>Login</legend>
 
-      <label for="regCompanyname">Companyname*</label>
-        <input type="text" name="regCompanyname" required>
+    <div class="regFormInput">
+      <label for="regCompanyname">Companyname</label>
+        <input type="text" name="regCompanyname">
+    </div>
 
+    <div class="regFormInput">
       <label for="regResidence">Residence*</label>
         <input type="text" name="regResidence" required>
+    </div>
 
+    <div class="regFormInput">
       <label for="regFirstname">Firstname*</label>
         <input type="text" name="regFirstname" required>
+    </div>
 
+    <div class="regFormInput">
       <label for="regLastname">Lastname*</label>
         <input type="text" name="regLastname" required>
+    </div>
 
+    <div class="regFormInput">
       <label for="regInsertion">Insertion</label>
         <input type="text" name="regInsertion">
+    </div>
 
+    <div class="regFormInput">
       <label for="regZipcode">Zipcode*</label>
         <input type="text" name="regZipcode" required>
+    </div>
 
+    <div class="regFormInput">
       <label for="regTelephone">Telephone*</label>
         <input type="text" name="regTelephone" required>
+    </div>
 
+    <div class="regFormInput">
       <label for="regEmail">Email*</label>
         <input type="email" name="regEmail" required>
+    </div> 
 
+    <div class="regFormInput">
       <label for="regPassword">Password*</label>
         <input type="password" name="regPassword" required>
+    </div>
 
+    <div class="regFormInput">
       <label for="regStreet">Street*</label>
         <input type="text" name="regStreet" required>
+    </div>
 
+    <div class="regFormInput">
       <label for="regHouse_number">Housenumber*</label>
         <input type="text" name="regHouse_number">
-        
-        <input type="submit" name="regSubmit" value="Register!">
-  </fieldset>
+    </div>
+
+    <input class="regSubmit" type="submit" name="regSubmit" value="Register!">
 </form>
--- Site/views/shop.view.php
+++ Site/views/shop.view.php
@@ -1,154 +1,172 @@
-<h1 class="productAreaTitle">Vlambeer Games</h1>
-<div class="productArea">
-	<div class="prevBtn">
-		<a href="#">
-			<img src="<?= ROOT."/assets/img/prevbtn.png" ?>"/>
-		</a>
-	</div>
-	<div class="products">
-		<div class="product1">
-			Product 1
-		</div>
-		<div class="product2">
-			Product 2
-		</div>
-		<div class="product3">
-			Product 3
-		</div>
-	</div>
-	<div class="nextBtn">
-		<a href="#">
-			<img src="<?= ROOT."/assets/img/nextbtn.png" ?>"/>
-		</a>
-	</div>
+</div> <!-- CLOSE CONTAINER -->
+
+<div class="shopSlider">
+	<!-- IMG -->
 </div>
 
-
-<h1 class="productAreaTitle">Special / Bundles</h1>
-<div class="productArea">
-	<div class="prevBtn">
-		<a href="#">
-			<img src="<?= ROOT."/assets/img/prevbtn.png" ?>"/>
-		</a>
-	</div>
-	<div class="products">
-		<div class="product1">
-			Product 1
+<div class="container">
+<!-- 
+	<h1 class="productAreaTitle">Vlambeer Games</h1>
+	<div class="productArea">
+		<div class="prevBtn">
+			<a href="#">
+				<img src="<?= ROOT."/assets/img/prevbtn.png" ?>"/>
+			</a>
 		</div>
-		<div class="product2">
-			Product 2
+		<div class="products">
+			<div class="product">
+				Product
+			</div>
+			<div class="product">
+				Product
+			</div>
+			<div class="product">
+				Product
+			</div>
 		</div>
-		<div class="product3">
-			Product 3
-		</div>
-	</div>
-	<div class="nextBtn">
-		<a href="#">
-			<img src="<?= ROOT."/assets/img/nextbtn.png" ?>"/>
-		</a>
-	</div>
-</div>
-
-
-<h1 class="productAreaTitle">Clothes</h1>
-<div class="productArea">
-	<div class="prevBtn">
-		<a href="#">
-			<img src="<?= ROOT."/assets/img/prevbtn.png" ?>"/>
-		</a>
-	</div>
-	<div class="products">
-		<div class="product1">
-			Product 1
-		</div>
-		<div class="product2">
-			Product 2
-		</div>
-		<div class="product3">
-			Product 3
+		<div class="nextBtn">
+			<a href="#">
+				<img src="<?= ROOT."/assets/img/nextbtn.png" ?>"/>
+			</a>
 		</div>
 	</div>
-	<div class="nextBtn">
-		<a href="#">
-			<img src="<?= ROOT."/assets/img/nextbtn.png" ?>"/>
-		</a>
-	</div>
-</div>
 
 
-<h1 class="productAreaTitle">Miscellaneous</h1>
-<div class="productArea">
-	<div class="prevBtn">
-		<a href="#">
-			<img src="<?= ROOT."/assets/img/prevbtn.png" ?>"/>
-		</a>
-	</div>
-	<div class="products">
-		<div class="product1">
-			Product 1
+	<h1 class="productAreaTitle">Special / Bundles</h1>
+	<div class="productArea">
+		<div class="prevBtn">
+			<a href="#">
+				<img src="<?= ROOT."/assets/img/prevbtn.png" ?>"/>
+			</a>
 		</div>
-		<div class="product2">
-			Product 2
+		<div class="products">
+			<div class="product">
+				Product
+			</div>
+			<div class="product">
+				Product
+			</div>
+			<div class="product">
+				Product
+			</div>
 		</div>
-		<div class="product3">
-			Product 3
+		<div class="nextBtn">
+			<a href="#">
+				<img src="<?= ROOT."/assets/img/nextbtn.png" ?>"/>
+			</a>
 		</div>
 	</div>
-	<div class="nextBtn">
-		<a href="#">
-			<img src="<?= ROOT."/assets/img/nextbtn.png" ?>"/>
-		</a>
-	</div>
-</div>
 
 
-<h1 class="productAreaTitle">Music</h1>
-<div class="productArea">
-	<div class="prevBtn">
-		<a href="#">
-			<img src="<?= ROOT."/assets/img/prevbtn.png" ?>"/>
-		</a>
-	</div>
-	<div class="products">
-		<div class="product1">
-			Product 1
+	<h1 class="productAreaTitle">Clothes</h1>
+	<div class="productArea">
+		<div class="prevBtn">
+			<a href="#">
+				<img src="<?= ROOT."/assets/img/prevbtn.png" ?>"/>
+			</a>
 		</div>
-		<div class="product2">
-			Product 2
+		<div class="products">
+			<div class="product">
+				Product
+			</div>
+			<div class="product">
+				Product
+			</div>
+			<div class="product">
+				Product
+			</div>
 		</div>
-		<div class="product3">
-			Product 3
+		<div class="nextBtn">
+			<a href="#">
+				<img src="<?= ROOT."/assets/img/nextbtn.png" ?>"/>
+			</a>
 		</div>
 	</div>
-	<div class="nextBtn">
-		<a href="#">
-			<img src="<?= ROOT."/assets/img/nextbtn.png" ?>"/>
-		</a>
-	</div>
-</div>
 
 
-<h1 class="productAreaTitle">Not-Vlambeer Games</h1>
-<div class="productArea">
-	<div class="prevBtn">
-		<a href="#">
-			<img src="<?= ROOT."/assets/img/prevbtn.png" ?>"/>
-		</a>
-	</div>
-	<div class="products">
-		<div class="product1">
-			Product 1
+	<h1 class="productAreaTitle">Miscellaneous</h1>
+	<div class="productArea">
+		<div class="prevBtn">
+			<a href="#">
+				<img src="<?= ROOT."/assets/img/prevbtn.png" ?>"/>
+			</a>
 		</div>
-		<div class="product2">
-			Product 2
+		<div class="products">
+			<div class="product">
+				Product
+			</div>
+			<div class="product">
+				Product
+			</div>
+			<div class="product">
+				Product
+			</div>
 		</div>
-		<div class="product3">
-			Product 3
+		<div class="nextBtn">
+			<a href="#">
+				<img src="<?= ROOT."/assets/img/nextbtn.png" ?>"/>
+			</a>
 		</div>
 	</div>
-	<div class="nextBtn">
-		<a href="#">
-			<img src="<?= ROOT."/assets/img/nextbtn.png" ?>"/>
-		</a>
+
+
+	<h1 class="productAreaTitle">Music</h1>
+	<div class="productArea">
+		<div class="prevBtn">
+			<a href="#">
+				<img src="<?= ROOT."/assets/img/prevbtn.png" ?>"/>
+			</a>
+		</div>
+		<div class="products">
+			<div class="product">
+				Product
+			</div>
+			<div class="product">
+				Product
+			</div>
+			<div class="product">
+				Product
+			</div>
+		</div>
+		<div class="nextBtn">
+			<a href="#">
+				<img src="<?= ROOT."/assets/img/nextbtn.png" ?>"/>
+			</a>
+		</div>
 	</div>
-</div>
+	-->
+
+	<h1 class="productAreaTitle">Not-Vlambeer Games</h1>
+	<div class="productArea">
+		<div class="prevBtn">
+			<a href="#">
+				<img src="<?= ROOT."/assets/img/prevbtn.png" ?>"/>
+			</a>
+		</div>
+		<div class="products">
+			<div class="product">
+				Product
+			</div>
+			<div class="product">
+				Product
+			</div>
+			<div class="product">
+				Product
+			</div>
+		</div>
+		<div class="nextBtn">
+			<a href="#">
+				<img src="<?= ROOT."/assets/img/nextbtn.png" ?>"/>
+			</a>
+		</div>
+	</div>
+
+	<div class="product-area">
+		<a href="#"><img src="<?= ASSETS ?>/img/prevbtn.png"></a>
+		<ul class="inner-container">
+			<li class="product-item"></li>
+			<li class="product-item"></li>
+			<li class="product-item"></li>
+		</ul>
+		<a href="#"><img src="<?= ASSETS ?>/img/nextbtn.png"></a>
+	</div>
